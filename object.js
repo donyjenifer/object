bd49e74f2bdd6a1c41cfea5ee31edaf39af62964
donyjenifer <jenifer.dony@gmail.com>
2023-06-02 12:39:04 +0530

    object.js

---
+++ object.js
@@ -0,0 +1,14 @@
+myObj={SmartAppliances:"Mobile"};
+myMobile={
+    phone:0000,
+app:{fun:"game",development:"Course"},
+site:["Google","Mozilla","Edge"],
+};
+action:function sample()
+{
+    return "Hello ";
+}
+
+console.log(myObj.SmartAppliances);
+console.log(myMobile.site[2]);
+console.log(sample());
